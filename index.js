e37660c1c450e25cd2e7a13540db673a313496f9
Bittu Gorai <bittukr20@gmail.com>
2022-12-25 22:12:11 +0530

    [bittu] Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import {migrate} from './lib/lib.mjs'
+
+const concurrency= 2;
+const destination_api_configs = {
+    url: "",
+    headers : {}
+}
+const sourceCsvFilePath = "sample.csv";
+const succesResutlsFilePath = "success.txt";
+const errorResultsFilePath = "error.txt";
+
+migrate({ sourceCsvFilePath, succesResutlsFilePath, errorResultsFilePath, destination_api_configs, concurrency});
