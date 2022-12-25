2dbf0f86930fc7483dda7132b361ddecc17f151a
bittukr20 <bittukr20@gmail.com>
2022-12-26 00:22:03 +0530

    [Bittu] Fix bugs

--- index.mjs
+++ index.mjs
@@ -2,17 +2,19 @@ import { migrate } from "./lib/lib.mjs";
 
 const concurrency = 2;
 const destination_api_configs = {
-  url: "",
+  url: "http://127.0.0.1:8080/url-shortener",
   headers: {},
+  method: "POST",
 };
 const sourceCsvFilePath = "sample.csv";
 const succesResutlsFilePath = "success.txt";
 const errorResultsFilePath = "error.txt";
-
+const number_of_items = 4;
 migrate({
   sourceCsvFilePath,
   succesResutlsFilePath,
   errorResultsFilePath,
   destination_api_configs,
   concurrency,
+  number_of_items,
 });
