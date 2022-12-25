e6bcab4098e078381fd894476083db8ac5fe16fe
bittukr20 <bittukr20@gmail.com>
2022-12-26 00:25:14 +0530

    [Bittu] modify all configs var in caps

--- index.mjs
+++ index.mjs
@@ -1,20 +1,21 @@
 import { migrate } from "./lib/lib.mjs";
 
-const concurrency = 2;
-const destination_api_configs = {
+const CONCURRENCY = 2;
+const DESTINATION_END_POINT_CONFIGS = {
   url: "http://127.0.0.1:8080/url-shortener",
   headers: {},
   method: "POST",
 };
-const sourceCsvFilePath = "sample.csv";
-const succesResutlsFilePath = "success.txt";
-const errorResultsFilePath = "error.txt";
-const number_of_items = 4;
+const SOURCE_CSV_FILE_PATH = "sample.csv";
+const SUCCESS_RESULTS_CSV_FILE_PATH = "success.txt";
+const ERROR_RESULTS_CSV_FILE_PATH = "error.txt";
+const TOTAL_NUMBER_OF_ITEMS = 4;
+
 migrate({
-  sourceCsvFilePath,
-  succesResutlsFilePath,
-  errorResultsFilePath,
-  destination_api_configs,
-  concurrency,
-  number_of_items,
+  sourceCsvFilePath: SOURCE_CSV_FILE_PATH,
+  succesResutlsFilePath: SUCCESS_RESULTS_CSV_FILE_PATH,
+  errorResultsFilePath: ERROR_RESULTS_CSV_FILE_PATH,
+  destination_api_configs: DESTINATION_END_POINT_CONFIGS,
+  concurrency: CONCURRENCY,
+  number_of_items: TOTAL_NUMBER_OF_ITEMS,
 });
