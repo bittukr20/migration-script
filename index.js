6ddd622f43935d93777d549e05733078ef4a3811
bittukr20 <bittukr20@gmail.com>
2022-12-25 22:37:24 +0530

    [bittu] set format on save

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-import {migrate} from './lib/lib.mjs'
+import { migrate } from "./lib/lib.mjs";
 
-const concurrency= 2;
+const concurrency = 2;
 const destination_api_configs = {
-    url: "",
-    headers : {}
-}
+  url: "",
+  headers: {},
+};
 const sourceCsvFilePath = "sample.csv";
 const succesResutlsFilePath = "success.txt";
 const errorResultsFilePath = "error.txt";
 
-migrate({ sourceCsvFilePath, succesResutlsFilePath, errorResultsFilePath, destination_api_configs, concurrency});
+migrate({
+  sourceCsvFilePath,
+  succesResutlsFilePath,
+  errorResultsFilePath,
+  destination_api_configs,
+  concurrency,
+});
